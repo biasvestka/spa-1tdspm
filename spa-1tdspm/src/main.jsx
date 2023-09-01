b37abb84e6798eb2523ab9b49aed3df237510b23
biasvestka <biasvestka@gmail.com>
2023-09-01 09:14:18 -0300

    Importando os componentes

--- spa-1tdspm/src/main.jsx
+++ spa-1tdspm/src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home.jsx';
 import EditarProdutos from './routes/EditarProdutos.jsx';
-import Erro404 from './routes/Erro404.jsx'; 
+import Erro404 from './routes/Erro404.jsx';
 import Produtos from './routes/Produtos.jsx';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
