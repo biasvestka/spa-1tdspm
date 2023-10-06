b6a6e4e6f5b6a67d46a5771a0c24769dc552fc9d
biasvestka <biasvestka@gmail.com>
2023-10-06 09:19:33 -0300

    Páginas corrigidas

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './routes/Home.jsx';
-import EditarProdutos from './routes/EditarProdutos.jsx';
-import Erro404 from './routes/Erro404.jsx';
-import Produtos from './routes/Produtos.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+import ExcluirProduto from './routes/ExcluirProduto.jsx'
+import Error404 from './routes/Error404.jsx'
+import InserirProdutos from './routes/InserirProdutos.jsx'
 
 const router = createBrowserRouter([
-  {path: '/', element: <App/>, 
-  errorElement: <Erro404/>,
-  children: [
-    {path: '/', element: <Home/>},
-    {path: '/produtos', element: <Produtos/>},
-    {path: '/editar/produtos/:id', element: <EditarProdutos/>},
-  ]}
+
+  {path: '/', element: <App/>,
+    errorElement: <Error404/>,
+    children:[
+      {path: '/', element: <Home/>}, 
+      {path: '/produtos', element: <Produtos/>}, 
+      {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+      {path: '/excluir/produtos/:id', element: <ExcluirProduto/>},
+      {path: '/cadastrar/produto/', element: <InserirProdutos/>}
+      
+    ]
+}
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    
-    <RouterProvider router={router}/>
-    
-  </React.StrictMode>,
-)
 
+        <RouterProvider router={router}/>
+  ,
+)
--- src/routes/Erro404.jsx
+++ src/routes/Erro404.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function Erro404() {
-
-  document.title = "Erro 404";
-
-  return (
-    <div>
-      <h1>Página não encontrada - 404</h1>
-      {/* <Link to="/">Voltar para a página inicial.</Link> */}
-        <p>Voltar para o início - <Link to="/">VOLTAR</Link></p>
-    </div>
-  )
+export default function Error404(){
+    document.title = "Erro 404";
+    
+    return(
+        <>
+        
+        <div>
+            <h1>Página não encontrada - 404</h1>
+            {/* <Link to="/">Voltar para a página inicial.</Link> */}
+            <p>Voltar para o in~icio - <Link to="/">Voltar</Link></p>
+        </div>
+        
+        </>
+    )
 }
